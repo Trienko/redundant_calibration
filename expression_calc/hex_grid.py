bd8dfa4f08404164d8f88ddce947601c47f25d8c
Trienko <trienkog@gmail.com>
2016-07-04 16:32:20 +0200

    end of day commit

--- expression_calc/hex_grid.py
+++ expression_calc/hex_grid.py
@@ -47,7 +47,12 @@ class Hgrid():
             y = y + (np.sqrt(3)/2.0)*l                 
             ant_main_row = ant_main_row - 1
     
-        return ant_x,ant_y 
+        return ant_x,ant_y
+
+    def calculate_phi(self,ant_x,ant_y):
+        for k in xrange(len(ant_x)):
+            for j in xrnage(k+1,len(ant_x)) 
+
 
 
 if __name__ == "__main__":
