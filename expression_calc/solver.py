1b3e4edcce2566d30d7d03b3d94b50ac3accf1b9
Trienko <trienkog@gmail.com>
2016-07-11 09:20:12 +0200

    testing general formulation

--- expression_calc/solver.py
+++ expression_calc/solver.py
@@ -95,6 +95,166 @@ class solver():
              plt.imshow(self.J_int,interpolation="nearest")
              plt.show() 
 
+
+      def hex_grid_ver2(self,hex_dim,l):
+          hex_dim = int(hex_dim)
+          side = int(hex_dim + 1)
+          ant_main_row = int(side + hex_dim)
+        
+          elements = 1
+
+          #summing the antennas in hexactoganal rings 
+          for k in xrange(hex_dim):
+              elements = elements + (k+1)*6
+                
+          ant_x = np.zeros((elements,),dtype=float)
+          ant_y = np.zeros((elements,),dtype=float)
+          print "len(ant_x) = ",len(ant_x)
+          print "len(ant_y) = ",len(ant_y)
+          x = 0.0
+          y = 0.0
+
+          counter = 0
+        
+          for k in xrange(side):
+              x_row = x
+              y_row = y
+              for i in xrange(ant_main_row):
+                  if k == 0:
+                     ant_x[counter] = x_row 
+                     ant_y[counter] = y_row
+                     x_row = x_row + l
+                     counter = counter + 1 
+                  else:
+                     ant_x[counter] = x_row
+                     ant_y[counter] = y_row
+                     counter = counter + 1
+                   
+                     ant_x[counter] = x_row
+                     ant_y[counter] = -1*y_row
+                     x_row = x_row + l
+                     counter = counter + 1   
+              x = x + l/2.0
+              y = y + (np.sqrt(3)/2.0)*l                 
+              ant_main_row = ant_main_row - 1
+       
+          y_idx = np.argsort(ant_y)
+          ant_y = ant_y[y_idx]
+          ant_x = ant_x[y_idx]
+
+          slice_value = int(side)
+          start_index = 0
+          add = True
+          ant_main_row = int(side + hex_dim)
+
+          for k in xrange(ant_main_row):
+              temp_vec_x = ant_x[start_index:start_index+slice_value]
+              x_idx = np.argsort(temp_vec_x)
+              temp_vec_x = temp_vec_x[x_idx]
+              ant_x[start_index:start_index+slice_value] = temp_vec_x
+              if slice_value == ant_main_row:
+                 add = False
+              start_index = start_index+slice_value 
+            
+              if add:
+                 slice_value = slice_value + 1
+              else:
+                 slice_value = slice_value - 1  
+
+              print "slice_value = ",slice_value
+              print "k = ",k  
+
+          return ant_x,ant_y
+
+      def square_grid(self,side,l):
+          elements = side*side
+                
+          ant_x = np.zeros((elements,),dtype=float)
+          ant_y = np.zeros((elements,),dtype=float)
+          print "len(ant_x) = ",len(ant_x)
+          print "len(ant_y) = ",len(ant_y)
+          x = 0.0
+          y = 0.0
+
+          counter = 0
+        
+          for k in xrange(side):
+              x = 0.0
+              for i in xrange(side):
+                  ant_x[counter] = x
+                  ant_y[counter] = y 
+                  x = x + l
+                  counter = counter + 1
+              y = y + l 
+ 
+          return ant_x,ant_y
+
+      def line_grid(self,elements,l):
+                       
+          ant_x = np.zeros((elements,),dtype=float)
+          ant_y = np.zeros((elements,),dtype=float)
+          print "len(ant_x) = ",len(ant_x)
+          print "len(ant_y) = ",len(ant_y)
+          x = 0.0
+          y = 0.0
+
+          for k in xrange(elements):
+              ant_x[k] = x
+              x = x + l
+
+          return ant_x,ant_y
+
+      def determine_phi_value(self,red_vec_x,red_vec_y,ant_x_p,ant_x_q,ant_y_p,ant_y_q):
+          red_x = ant_x_q - ant_x_p
+          red_y = ant_y_q - ant_y_p
+
+          for l in xrange(len(red_vec_x)):
+              if (np.allclose(red_x,red_vec_x[l]) and np.allclose(red_y,red_vec_y[l])):
+                 return red_vec_x,red_vec_y,l+1
+
+          red_vec_x = np.append(red_vec_x,np.array([red_x]))
+          red_vec_y = np.append(red_vec_y,np.array([red_y]))
+          return red_vec_x,red_vec_y,len(red_vec_x) 
+
+      def calculate_phi(self,ant_x,ant_y,plot=True):
+          phi = np.zeros((len(ant_x),len(ant_y)))
+          zeta = np.zeros((len(ant_x),len(ant_y)))
+          red_vec_x = np.array([])
+          red_vec_y = np.array([])
+          for k in xrange(len(ant_x)):
+              for j in xrange(k+1,len(ant_x)):
+                  red_vec_x,red_vec_y,phi[k,j]  = self.determine_phi_value(red_vec_x,red_vec_y,ant_x[k],ant_x[j],ant_y[k],ant_y[j])           
+                  zeta[k,j] phi[k,j]
+                  zeta[j,k] = zeta[k,j]
+                  #phi[j,k] = phi[k,j]
+
+          if plot:
+             plt.imshow(phi,interpolation="nearest")
+             x = np.arange(len(ant_x))
+             plt.xticks(x, x+1)
+             y = np.arange(len(ant_x))
+             plt.yticks(y, y+1)
+             plt.colorbar() 
+             #plt.yticks(y, y+1)
+           
+             plt.show()
+
+          print "phi = ",phi
+          return phi,zeta
+
+      '''
+      INPUTS:
+      z - Input vector.
+      N - Number of elements in array.
+      plot - Plot the resultant matrices if True.     
+      '''
+      def generate_H_formula(self,z,N,plot=False):
+          g = z[:N]
+          y = z[N:]
+           
+
+         
+
       '''
       Creates the redundant Hessian from variables. Stores the result in self.H_sym
       
