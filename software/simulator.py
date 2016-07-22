8c962964da858ac932f8b18a7eeedaec7f724416
Trienko <trienkog@gmail.com>
2016-07-22 10:39:57 +0200

    starting new code to help with newest attempt for paper

---
+++ software/simulator.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pylab as plt
+
+'''
+Simulator that can create uv-tracks and scalar visibilities associated with a regular east-west array
+'''
+
+class sim():
+
+      '''
+      Constructor
+      '''
+      def __init__(self):
+          pass
+
+if __name__ == "__main__":
+   N = 6
+   s = sim()
