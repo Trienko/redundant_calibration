6cc307eb1b32484edf63f8a5e0c2197db00f5a4c
Trienko <trienkog@gmail.com>
2016-07-22 16:08:44 +0200

    simulator.m

--- software/simulator.py
+++ software/simulator.py
@@ -34,7 +34,7 @@ class sim():
       order - Fundamental parameter which determines the layout size
       '''
       
-      def __init__(self,h_min=-6,h_max=6,dec=(-74-39./60-37.481)*(np.pi/180),lat=(-30 - 43.0/60.0 - 17.34/3600)*(np.pi/180),freq=1.4*10**9,layout="KAT7.txt",nsteps=300,bas_len=None,order=None):
+      def __init__(self,h_min=-6,h_max=6,dec=(-74-39./60-37.481)*(np.pi/180),lat=(-30 - 43.0/60.0 - 17.34/3600)*(np.pi/180),freq=1.4*10**9,layout="WSRT.txt",nsteps=300,bas_len=None,order=None):
           self.h_min = h_min
           self.h_max = h_max
           self.nsteps = nsteps
@@ -51,6 +51,9 @@ class sim():
           self.N = 0 #NUMEBR OF ANTENNAS
           self.B = 0 #NUMBER OF BASELINES
           self.L = 0 #NUMBER OF REDNUNDANT BASELINES
+          self.u_m = None #The uvw composite index matrices
+          self.v_m = None
+          self.w_m = None
 
       '''
       Reads the antenna layout from a text file
@@ -161,8 +164,8 @@ class sim():
       def uv_track(self,h,d,az,el,lat,dec,wave_length):
           uvw = np.zeros((len(h),3))
           for i in xrange(len(h)):
-              A = XYZ_to_uvw(h[i],dec)
-              uvw[i,:] = A.dot(DAE_to_XYZ(d,az,el,lat)/wave_length)
+              A = self.XYZ_to_uvw(h[i],dec)
+              uvw[i,:] = A.dot(self.DAE_to_XYZ(d,az,el,lat)/wave_length)
           return uvw 
       
 
@@ -203,8 +206,8 @@ class sim():
           self.w_m = np.zeros((self.N,self.N,self.nsteps),dtype=float)    
    
           
-          for i in xrange(num_baselines):
-              X = uv_track(self.h,DAE[i,0],DAE[i,1],DAE[i,2],self.lat,self.dec,self.wave)
+          for i in xrange(self.B):
+              X = self.uv_track(self.h,DAE[i,0],DAE[i,1],DAE[i,2],self.lat,self.dec,self.wave)
               self.u_m[p[i],q[i],:] = X[:,0]
               self.u_m[q[i],p[i],:] = -1*X[:,0]
               self.v_m[p[i],q[i],:] = X[:,1]
@@ -222,17 +225,17 @@ class sim():
       labelsize - font size of labels
       None
       ''' 
-      def plot_uv_coverage(self,title,label_size):
-          m_x = np.amax(np.absolute(u_m))
-          m_y = np.amax(np.absolute(v_m)) 
+      def plot_uv_coverage(self,title="KAT7",label_size=14):
+          m_x = np.amax(np.absolute(self.u_m))
+          m_y = np.amax(np.absolute(self.v_m)) 
           for i in xrange(self.N):
               for j in xrange(i+1,self.N):
                   plt.plot(self.u_m[i,j,:],self.v_m[i,j,:],"b")  
                   plt.plot(self.u_m[j,i,:],self.v_m[j,i,:],"r")
           plt.ylim(-1.1*m_y,1.1*m_y)
           plt.xlim(-1.1*m_x,1.1*m_x)
-          plt.xlabel("u [$\lambda$]",fontsize=label_size)
-          plt.ylabel("v [$\lambda$]",fontsize=label_size)
+          plt.xlabel("$u$ [$\lambda$]",fontsize=label_size)
+          plt.ylabel("$v$ [$\lambda$]",fontsize=label_size)
           plt.title(title,fontsize=label_size)
           plt.show() 
           
@@ -241,6 +244,6 @@ class sim():
 if __name__ == "__main__":
    s = sim()
    s.read_antenna_layout()
-   s.plot_ant()
+   s.plot_ant(title="WSRT")
    s.uv_tracks()
-   s.plot_uv_coverage()
+   s.plot_uv_coverage(title="WSRT")
