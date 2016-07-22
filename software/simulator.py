4ac536490f88f0d722a6779d689b94e4920f82e9
Trienko <trienkog@gmail.com>
2016-07-22 12:19:54 +0200

    added antenna plot function

--- software/simulator.py
+++ software/simulator.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pylab as plt
+import matplotlib as mpl
 
 '''
 Simulator that can create uv-tracks and basic scalar visibilities.
@@ -37,13 +38,12 @@ class sim():
           self.h_min = h_min
           self.h_max = h_max
           self.nsteps = nsteps
-          self.h = np.linspace(self.h_min,self.h_max,num=nstep)*np.pi/12
+          self.h = np.linspace(self.h_min,self.h_max,num=nsteps)*np.pi/12
           self.dec = dec
           self.lat = lat
           self.freq = freq
           self.wave = (3*10**8)/self.freq
           self.layout = layout
-          self.nsteps = nsteps
           self.bas_len = bas_len
           self.order = order
 
@@ -61,11 +61,11 @@ class sim():
       INPUTS:
       None
       ''' 
-      def read_antenna_layout():
+      def read_antenna_layout(self):
           file = open(self.layout, 'r')
           num_ant = file.read().count("\n")
           file.close()
-          file = open(Layout, 'r')
+          file = open(self.layout, 'r')
           array_l = np.zeros((num_ant,3))#N E Up (m)
           counter = 0
           for line in file:
@@ -78,10 +78,38 @@ class sim():
                  array_l[counter,2] = float(val[2])
               counter = counter + 1
           file.close()
-          print "array_l = ",array_l
+          #print "array_l = ",array_l
           self.ant = array_l
-           
+
+      '''
+      Plots the antenna layout
+
+      RETURNS:
+      None
+
+      INPUTS:
+      title - title of plot
+      labelsize - font size of labels
+      ms - marker size
+      '''
+      def plot_ant(self,title="KAT7",label_size=14,ms=10):
+          m = np.amax(np.absolute(self.ant))
+          mpl.rcParams['xtick.labelsize'] = label_size 
+          mpl.rcParams['ytick.labelsize'] = label_size 
+          plt.plot(self.ant[:,0],self.ant[:,1],'bo',ms=ms)
+          #labels = ["0","1","2","3","4","5","6"]
+          #for label, x, y in zip(labels, array_l[:, 0], array_l[:, 1]):
+              #plt.annotate(label, xy = (x, y), xytext = (-15, 15), textcoords = 'offset points', fontsize = 12, ha = 'right', va = 'bottom', bbox = dict(boxstyle = 'round,pad=0.5', fc = 'white', alpha = 0.2), arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0')) 
+          plt.xlabel("W-E [m]",fontsize=label_size)
+          plt.ylabel("S-N [m]",fontsize=label_size)
+          plt.title(title,fontsize=label_size)
+    
+          plt.ylim(-1.1*m,1.1*m)
+          plt.xlim(-1.1*m,1.1*m)
+          plt.show()  
 
 
 if __name__ == "__main__":
    s = sim()
+   s.read_antenna_layout()
+   s.plot_ant()
