cff987f9d195ce4d5eecd3a15a488675b3b40cde
Trienko <trienkog@gmail.com>
2016-09-14 15:11:29 +0200

    developed the plotting of the three graphs

---
+++ software/plot_func.py
@@ -0,0 +1,370 @@
+import numpy as np
+import scipy as sp
+import pylab as plt
+import simulator
+import analytic
+from scipy.sparse import csr_matrix
+from scipy.sparse.linalg import cg
+from scipy.linalg import pinv
+from scipy.sparse import dia_matrix
+from scipy.stats import mode
+import time
+import pickle
+import os
+import sys, getopt
+from scipy.stats import gaussian_kde
+#from statistics import mode
+import matplotlib
+
+def kde_scipy(x, x_grid, bandwidth=0.2, **kwargs):
+    """Kernel Density Estimation with Scipy"""
+    # Note that scipy weights its bandwidth by the covariance of the
+    # input data.  To make the results comparable to the other methods,
+    # we divide the bandwidth by the sample standard deviation here.
+    #kde = gaussian_kde(x, bw_method=bandwidth / x.std(ddof=1), **kwargs)
+    kde = gaussian_kde(x)
+    return kde.evaluate(x_grid)
+
+def plot_kappa_itr(SNR=10,k_upper=1):
+    
+    N = np.array([7,19,37,61,91])
+
+    kappa_pcg_mean = np.zeros((len(N),),dtype=float)
+    itr_pcg_mean = np.zeros((len(N),),dtype=float)
+    kappa_cg_median = np.zeros((len(N),),dtype=float)
+    itr_cg_median = np.zeros((len(N),),dtype=float)
+
+    kappa_pcg_std = np.zeros((len(N),),dtype=float)
+    itr_pcg_std = np.zeros((len(N),),dtype=float)
+    kappa_cg_mad = np.zeros((len(N),),dtype=float)
+    itr_cg_mad = np.zeros((len(N),),dtype=float)
+
+    kappa_pcg_dic = {}
+    kappa_cg_dic = {}
+    itr_pcg_dic = {}
+    itr_cg_dic = {}
+
+    for j in xrange(len(N)):
+        kappa_pcg_dic[str(N[j])]=np.array([],dtype=float)
+        kappa_cg_dic[str(N[j])]=np.array([],dtype=float)
+        itr_pcg_dic[str(N[j])]=np.array([],dtype=float) 
+        itr_cg_dic[str(N[j])]=np.array([],dtype=float)     
+
+    print "k = ",kappa_pcg_dic[str(N[0])]    
+
+    for k in xrange(k_upper):
+        PCG_DIR = "./HEX_PCG_"+str(SNR)+"_False_"+str(k)
+        PCG_FILE_LIST = np.array([PCG_DIR+"/1_7_9_"+PCG_DIR[2:]+".p",PCG_DIR+"/2_19_30_"+PCG_DIR[2:]+".p",PCG_DIR+"/3_37_63_"+PCG_DIR[2:]+".p",PCG_DIR+"/4_61_108_"+PCG_DIR[2:]+".p",PCG_DIR+"/5_91_165_"+PCG_DIR[2:]+".p"]) 
+        CG_DIR = "./HEX_CG_"+str(SNR)+"_False_"+str(k)
+        CG_FILE_LIST = np.array([CG_DIR+"/1_7_9_"+CG_DIR[2:]+".p",CG_DIR+"/2_19_30_"+CG_DIR[2:]+".p",CG_DIR+"/3_37_63_"+CG_DIR[2:]+".p",CG_DIR+"/4_61_108_"+CG_DIR[2:]+".p",CG_DIR+"/5_91_165_"+CG_DIR[2:]+".p"])
+        for i in xrange(len(PCG_FILE_LIST)):
+            #LOAD PCG
+            print "PCG_FILE_LIST = ",PCG_FILE_LIST[i]
+            file_p = open(PCG_FILE_LIST[i], 'rb')
+            order = pickle.load(file_p)
+            N_v = pickle.load(file_p)
+            L = pickle.load(file_p)
+            zeta = pickle.load(file_p)
+            PQ = pickle.load(file_p)
+            z_cal = pickle.load(file_p)
+            c_cal = pickle.load(file_p)
+            time_mat = pickle.load(file_p)
+            outer_loop = pickle.load(file_p)
+            #print "outer_loop.shape = ",outer_loop.shape
+            itr_vec = pickle.load(file_p)
+            kappa_vec = pickle.load(file_p)
+            kappa_pcg_dic[str(N[i])] = np.append(kappa_pcg_dic[str(N[i])],kappa_vec)
+            itr_pcg_dic[str(N[i])] = np.append(itr_pcg_dic[str(N[i])],itr_vec)
+            file_p.close()
+
+            #LOAD CG
+            print "CG_FILE_LIST = ",CG_FILE_LIST[i]
+            file_p = open(CG_FILE_LIST[i], 'rb')
+            order = pickle.load(file_p)
+            N_v = pickle.load(file_p)
+            L = pickle.load(file_p)
+            zeta = pickle.load(file_p)
+            PQ = pickle.load(file_p)
+            z_cal = pickle.load(file_p)
+            c_cal = pickle.load(file_p)
+            time_mat = pickle.load(file_p)
+            outer_loop = pickle.load(file_p)
+            itr_vec = pickle.load(file_p)
+            kappa_vec = pickle.load(file_p)
+            print "s = ",str(N[i])
+            t_kappa = kappa_cg_dic[str(N[i])]
+            t_itr = itr_cg_dic[str(N[i])]
+            kappa_cg_dic[str(N[i])] = np.append(t_kappa,kappa_vec)
+            itr_cg_dic[str(N[i])] = np.append(t_itr,itr_vec)
+            file_p.close()
+    
+    for n in xrange(len(N)):
+        kappa_pcg_mean[n] = np.mean(kappa_pcg_dic[str(N[n])])
+        itr_pcg_mean[n] = np.mean(itr_pcg_dic[str(N[n])])
+        kappa_cg_median[n] = np.median(kappa_cg_dic[str(N[n])])
+        itr_cg_median[n] = np.median(itr_cg_dic[str(N[n])])
+
+        kappa_pcg_std[n] = np.std(kappa_pcg_dic[str(N[n])])
+        itr_pcg_std[n] = np.std(itr_pcg_dic[str(N[n])])
+        kappa_cg_mad[n] = np.median(np.absolute(kappa_cg_dic[str(N[n])] - np.median(kappa_cg_dic[str(N[n])])))
+        itr_cg_mad[n] = np.median(np.absolute(itr_cg_dic[str(N[n])] - np.median(itr_cg_dic[str(N[n])])))
+    
+
+    #print "kappa_cg_mad = ",kappa_cg_mad
+    #print "itr_cg_mad = ",itr_cg_mad
+
+    #print "N = ",N
+    matplotlib.rcParams.update({'font.size': 22})
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.plot(N,itr_cg_median,"r",lw=2,label="CG")
+    ax.fill_between(N, itr_cg_median-itr_cg_mad, itr_cg_median+itr_cg_mad,alpha=0.2, edgecolor='k', facecolor='r')
+    ax.plot(N,itr_pcg_mean,"b",lw=2,label="PCG")
+    ax.fill_between(N, itr_pcg_mean-itr_pcg_std, itr_pcg_mean+itr_pcg_std,alpha=0.2, edgecolor='k', facecolor='b')
+    ax.set_yscale('log')
+    ax.set_xlabel(r'$N$')
+    ax.set_ylabel('Iterations required by CG/PCG')
+    ax.legend(loc=5)
+    plt.show()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.plot(N,kappa_cg_median,"r",lw=2,label="CG")
+    ax.fill_between(N, kappa_cg_median-kappa_cg_mad, kappa_cg_median+kappa_cg_mad,alpha=0.2, edgecolor='k', facecolor='r')
+    ax.plot(N,kappa_pcg_mean,"b",lw=2,label="PCG")
+    ax.set_yscale('log')
+    ax.set_xlabel(r'$N$')
+    ax.set_ylabel(r'$\kappa$')
+    ax.legend(loc=5)
+    plt.show()
+    '''
+    print "len(itr_vec) = ",len(itr_vec)
+    print "len(kappa_vec) = ",len(kappa_vec)
+    print "mean(itr_vec) = ",np.mean(itr_vec)
+    print "std(itr_vec) = ",np.std(itr_vec)
+    print "mean(kappa_vec) = ",np.mean(kappa_vec)
+    print "std(kappa_vec) = ",np.std(kappa_vec)
+    print "amax(kappa_vec) = ",np.amax(kappa_vec)
+    print "amin(kappa_vec) = ",np.amin(kappa_vec)
+    print "median(kappa_vec) = ",np.median(kappa_vec)
+    print "mad = ",np.median(np.absolute(kappa_vec - np.median(kappa_vec)))
+    print "c_cal = ",c_cal
+
+    x_grid = np.linspace(0,200000,1000)
+ 
+    y = kde_scipy(kappa_vec[kappa_vec<200000], x_grid)
+    
+    plt.plot(x_grid,y)
+    plt.show()
+
+    #n, bins, patches = plt.hist(kappa_vec[kappa_vec<100000], 200, normed=1, facecolor='green', alpha=0.75)
+    #plt.show()
+
+    G_cal = pickle.load(file_p)
+    M_cal = pickle.load(file_p)
+    D = pickle.load(file_p)
+    M = pickle.load(file_p)
+    file_p.close()
+    '''
+
+def plot_outer_loop(SNR=10,k_upper=1):
+    
+    N = np.array([7,19,37,61,91])
+
+    outerloop_pcg_mean = np.zeros((len(N),),dtype=float)
+    outerloop_stef_mean = np.zeros((len(N),),dtype=float)
+
+    outerloop_pcg_std = np.zeros((len(N),),dtype=float)
+    outerloop_stef_std = np.zeros((len(N),),dtype=float)
+    
+    outerloop_pcg_dic = {}
+    outerloop_stef_dic = {}
+    
+    for j in xrange(len(N)):
+        outerloop_pcg_dic[str(N[j])]=np.array([],dtype=float)
+        outerloop_stef_dic[str(N[j])]=np.array([],dtype=float)
+
+    for k in xrange(k_upper):
+        PCG_DIR = "./HEX_PCG_"+str(SNR)+"_False_"+str(k)
+        PCG_FILE_LIST = np.array([PCG_DIR+"/1_7_9_"+PCG_DIR[2:]+".p",PCG_DIR+"/2_19_30_"+PCG_DIR[2:]+".p",PCG_DIR+"/3_37_63_"+PCG_DIR[2:]+".p",PCG_DIR+"/4_61_108_"+PCG_DIR[2:]+".p",PCG_DIR+"/5_91_165_"+PCG_DIR[2:]+".p"]) 
+        STEF_DIR = "./HEX_R_StEFCal_"+str(SNR)+"_"+str(k)
+        STEF_FILE_LIST = np.array([STEF_DIR+"/1_7_9_"+STEF_DIR[2:]+".p",STEF_DIR+"/2_19_30_"+STEF_DIR[2:]+".p",STEF_DIR+"/3_37_63_"+STEF_DIR[2:]+".p",STEF_DIR+"/4_61_108_"+STEF_DIR[2:]+".p",STEF_DIR+"/5_91_165_"+STEF_DIR[2:]+".p"])
+        for i in xrange(len(PCG_FILE_LIST)):
+            #LOAD PCG
+            print "PCG_FILE_LIST = ",PCG_FILE_LIST[i]
+            file_p = open(PCG_FILE_LIST[i], 'rb')
+            order = pickle.load(file_p)
+            N_v = pickle.load(file_p)
+            L = pickle.load(file_p)
+            zeta = pickle.load(file_p)
+            PQ = pickle.load(file_p)
+            z_cal = pickle.load(file_p)
+            c_cal = pickle.load(file_p)
+            time_mat = pickle.load(file_p)
+            outer_loop = pickle.load(file_p)
+            outerloop_pcg_dic[str(N[i])] = np.append(outerloop_pcg_dic[str(N[i])],outer_loop)
+            file_p.close()
+
+            #LOAD STEF
+            print "STEF_FILE_LIST = ",STEF_FILE_LIST[i]
+            file_p = open(STEF_FILE_LIST[i], 'rb')
+            order = pickle.load(file_p)
+            N_v = pickle.load(file_p)
+            L = pickle.load(file_p)
+            zeta = pickle.load(file_p)
+            z_cal = pickle.load(file_p)
+            c_cal = pickle.load(file_p)
+            time_mat = pickle.load(file_p)
+            outer_loop = pickle.load(file_p)
+            outerloop_stef_dic[str(N[i])] = np.append(outerloop_stef_dic[str(N[i])],outer_loop)
+            file_p.close()
+    
+    for n in xrange(len(N)):
+        outerloop_pcg_vec = outerloop_pcg_dic[str(N[n])]
+        outerloop_pcg_mean[n] = np.median(outerloop_pcg_vec[outerloop_pcg_vec<=9999])
+        outerloop_stef_vec = outerloop_stef_dic[str(N[n])]
+        outerloop_stef_mean[n] = np.median(outerloop_stef_vec[outerloop_stef_vec<=9999])
+        
+        outerloop_pcg_std[n] = np.median(np.absolute(outerloop_pcg_vec[outerloop_pcg_vec<=9999] - np.median(outerloop_pcg_vec[outerloop_pcg_vec<=9999])))
+        outerloop_stef_std[n] = np.median(np.absolute(outerloop_stef_vec[outerloop_stef_vec<=9999] - np.median(outerloop_stef_vec[outerloop_stef_vec<=9999])))
+    
+
+    #print "kappa_cg_mad = ",kappa_cg_mad
+    #print "itr_cg_mad = ",itr_cg_mad
+
+    #print "N = ",N
+    matplotlib.rcParams.update({'font.size': 22})
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.plot(N,outerloop_pcg_mean,"r",lw=2,label="SPARC")
+    ax.fill_between(N,outerloop_pcg_mean-outerloop_pcg_std, outerloop_pcg_mean+outerloop_pcg_std,alpha=0.2, edgecolor='k', facecolor='r')
+    ax.plot(N,outerloop_stef_mean,"b",lw=2,label="R-StEFCal")
+    ax.fill_between(N, outerloop_stef_mean-outerloop_stef_std, outerloop_stef_mean+outerloop_stef_std,alpha=0.2, edgecolor='k', facecolor='b')
+    ax.set_yscale('log')
+    ax.set_xlabel(r'$N$')
+    ax.set_ylabel('Iterations required by R-StEFCal/SPARC')
+    ax.legend(loc=5)
+    plt.show()
+
+
+def plot_time(SNR=1000,k_upper=1):
+    
+    N = np.array([7,19,37])
+
+    time_pcg_mean = np.zeros((len(N),),dtype=float)
+    time_stef_mean = np.zeros((len(N),),dtype=float)
+    time_svd_mean = np.zeros((len(N),),dtype=float)
+
+    time_pcg_std = np.zeros((len(N),),dtype=float)
+    time_stef_std = np.zeros((len(N),),dtype=float)
+    time_svd_std = np.zeros((len(N),),dtype=float)    
+
+    time_pcg_dic = {}
+    time_stef_dic = {}
+    time_svd_dic = {}    
+
+    for j in xrange(len(N)):
+        time_pcg_dic[str(N[j])]=np.array([],dtype=float)
+        time_stef_dic[str(N[j])]=np.array([],dtype=float)
+        time_svd_dic[str(N[j])]=np.array([],dtype=float)
+
+    for k in xrange(k_upper):
+        PCG_DIR = "./HEX_PCG_"+str(SNR)+"_True_"+str(k)
+        PCG_FILE_LIST = np.array([PCG_DIR+"/1_7_9_"+PCG_DIR[2:]+".p",PCG_DIR+"/2_19_30_"+PCG_DIR[2:]+".p",PCG_DIR+"/3_37_63_"+PCG_DIR[2:]+".p"]) 
+        #PCG_FILE_LIST = np.array([PCG_DIR+"/1_7_9_"+PCG_DIR[2:]+".p",PCG_DIR+"/2_19_30_"+PCG_DIR[2:]+".p"]) 
+        STEF_DIR = "./HEX_R_StEFCal_"+str(SNR)+"_"+str(k)
+        STEF_FILE_LIST = np.array([STEF_DIR+"/1_7_9_"+STEF_DIR[2:]+".p",STEF_DIR+"/2_19_30_"+STEF_DIR[2:]+".p",STEF_DIR+"/3_37_63_"+STEF_DIR[2:]+".p"])
+        #STEF_FILE_LIST = np.array([STEF_DIR+"/1_7_9_"+STEF_DIR[2:]+".p",STEF_DIR+"/2_19_30_"+STEF_DIR[2:]+".p"])
+        SVD_DIR = "./HEX_SVD_"+str(SNR)+"_True_"+str(k)
+        SVD_FILE_LIST = np.array([SVD_DIR+"/1_7_9_"+SVD_DIR[2:]+".p",SVD_DIR+"/2_19_30_"+SVD_DIR[2:]+".p",SVD_DIR+"/3_37_63_"+SVD_DIR[2:]+".p"])
+        #SVD_FILE_LIST = np.array([SVD_DIR+"/1_7_9_"+SVD_DIR[2:]+".p",SVD_DIR+"/2_19_30_"+SVD_DIR[2:]+".p"])
+        for i in xrange(len(PCG_FILE_LIST)):
+            #LOAD PCG
+            print "PCG_FILE_LIST = ",PCG_FILE_LIST[i]
+            file_p = open(PCG_FILE_LIST[i], 'rb')
+            order = pickle.load(file_p)
+            N_v = pickle.load(file_p)
+            L = pickle.load(file_p)
+            zeta = pickle.load(file_p)
+            PQ = pickle.load(file_p)
+            z_cal = pickle.load(file_p)
+            c_cal = pickle.load(file_p)
+            time_mat = pickle.load(file_p)
+            outer_loop = pickle.load(file_p)
+            t = (time_mat[1,:] - time_mat[0,:])/(1.0*outer_loop)
+            time_pcg_dic[str(N[i])] = np.append(time_pcg_dic[str(N[i])],t)
+            file_p.close()
+
+            #LOAD STEF
+            print "STEF_FILE_LIST = ",STEF_FILE_LIST[i]
+            file_p = open(STEF_FILE_LIST[i], 'rb')
+            order = pickle.load(file_p)
+            N_v = pickle.load(file_p)
+            L = pickle.load(file_p)
+            zeta = pickle.load(file_p)
+            z_cal = pickle.load(file_p)
+            c_cal = pickle.load(file_p)
+            time_mat = pickle.load(file_p)
+            outer_loop = pickle.load(file_p)
+            t = (time_mat[1,:] - time_mat[0,:])/(1.0*outer_loop)
+            time_stef_dic[str(N[i])] = np.append(time_stef_dic[str(N[i])],t)
+            file_p.close()
+
+            #LOAD SVD
+            print "SVD_FILE_LIST = ",SVD_FILE_LIST[i]
+            file_p = open(SVD_FILE_LIST[i], 'rb')
+            order = pickle.load(file_p)
+            N_v = pickle.load(file_p)
+            L = pickle.load(file_p)
+            zeta = pickle.load(file_p)
+            PQ = pickle.load(file_p)
+            z_cal = pickle.load(file_p)
+            c_cal = pickle.load(file_p)
+            time_mat = pickle.load(file_p)
+            outer_loop = pickle.load(file_p)
+            t = (time_mat[1,:] - time_mat[0,:])/(1.0*outer_loop)
+            time_svd_dic[str(N[i])] = np.append(time_svd_dic[str(N[i])],t)
+            file_p.close()
+    
+    for n in xrange(len(N)):
+        time_pcg_mean[n] = np.mean(time_pcg_dic[str(N[n])])
+        time_stef_mean[n] = np.mean(time_stef_dic[str(N[n])])
+        time_svd_mean[n] = np.mean(time_svd_dic[str(N[n])])
+
+        time_pcg_std[n] = np.std(time_pcg_dic[str(N[n])])
+        time_stef_std[n] = np.std(time_stef_dic[str(N[n])])
+        time_svd_std[n] = np.std(time_svd_dic[str(N[n])])
+        
+        
+    #print "kappa_cg_mad = ",kappa_cg_mad
+    #print "itr_cg_mad = ",itr_cg_mad
+
+    #print "N = ",N
+    matplotlib.rcParams.update({'font.size': 22})
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.plot(N,time_pcg_mean,"r",lw=2,label="PCG")
+    ax.fill_between(N,time_pcg_mean-time_pcg_std, time_pcg_mean+time_pcg_std,alpha=0.2, edgecolor='k', facecolor='r')
+    #z = np.polyfit(N, time_pcg_mean, 2)
+    #ax.plot(N,z[0]*N**2+z[1]*N+z[0],"r--")
+    #print "z = ",z
+    ax.plot(N,time_stef_mean,"b",lw=2,label="R-StEFCal")
+    ax.fill_between(N, time_stef_mean-time_stef_std, time_stef_mean+time_stef_std,alpha=0.2, edgecolor='k', facecolor='b')
+    #z = np.polyfit(N, time_stef_mean, 2)
+    #ax.plot(N,z[0]*N**2,"b--")
+    #print "z = ",z
+    ax.plot(N,time_svd_mean,"g",lw=2,label="SVD")
+    ax.fill_between(N, time_svd_mean-time_svd_std, time_svd_mean+time_svd_std,alpha=0.2, edgecolor='k', facecolor='g')
+    #z = np.polyfit(N, time_svd_mean, 3)
+    #ax.plot(N,z[0]*N**3,"g--")
+    #print "z = ",z
+    #ax.set_yscale('log')
+    ax.set_xlabel(r'$N$')
+    ax.set_ylabel('Average Execution Time per outer loop [$s$]')
+    ax.legend(loc=2)
+    plt.show()
+
+if __name__ == "__main__":
+   #plot_kappa_itr()
+   #plot_outer_loop()
+   plot_time()
