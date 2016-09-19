f88ca8e12fb336028ecba07f0cdf5a910fd598fb
Trienko <trienkog@gmail.com>
2016-09-19 17:18:53 +0200

    plot the function

--- software/plot_func.py
+++ software/plot_func.py
@@ -243,15 +243,15 @@ def plot_outer_loop(SNR=10,k_upper1=5,k_upper2=5):
         outerloop_pcg_vec = outerloop_pcg_dic[str(N[n])]
         
         print "N = ",N[n]
-        print "len(outerloop_pcg_vec) = ",len(outerloop_pcg_vec[outerloop_pcg_vec<=9999])
+        print "len(outerloop_pcg_vec) = ",len(outerloop_pcg_vec[outerloop_pcg_vec<=9998])
                   
         if len(outerloop_pcg_vec) == 0:
-           break
-        outerloop_pcg_mean[n] = np.median(outerloop_pcg_vec[outerloop_pcg_vec<=9999])
+           break8
+        outerloop_pcg_mean[n] = np.median(outerloop_pcg_vec[outerloop_pcg_vec<=9998])
         outerloop_stef_vec = outerloop_stef_dic[str(N[n])]
 
-        print "len(outerloop_stef_vec) = ",len(outerloop_stef_vec[outerloop_stef_vec<=9999])
-        temp_v = outerloop_stef_vec[outerloop_stef_vec<=9999]
+        print "len(outerloop_stef_vec) = ",len(outerloop_stef_vec[outerloop_stef_vec<=9998])
+        temp_v = outerloop_stef_vec[outerloop_stef_vec<=9998]
         temp_v = outerloop_stef_vec[temp_v>9000]
         print "len(temp_v) = ",len(temp_v)              
    
@@ -259,12 +259,12 @@ def plot_outer_loop(SNR=10,k_upper1=5,k_upper2=5):
         if len(outerloop_stef_vec) == 0:
            break
         
-        outerloop_stef_mean[n] = np.median(outerloop_stef_vec[outerloop_stef_vec<=9999])
+        outerloop_stef_mean[n] = np.median(outerloop_stef_vec[outerloop_stef_vec<=9998])
 
         print "outerloop_stef_mean = ",outerloop_stef_mean
                 
-        outerloop_pcg_std[n] = np.median(np.absolute(outerloop_pcg_vec[outerloop_pcg_vec<=9999] - np.median(outerloop_pcg_vec[outerloop_pcg_vec<=9999])))
-        outerloop_stef_std[n] = np.median(np.absolute(outerloop_stef_vec[outerloop_stef_vec<=9999] - np.median(outerloop_stef_vec[outerloop_stef_vec<=9999])))
+        outerloop_pcg_std[n] = np.median(np.absolute(outerloop_pcg_vec[outerloop_pcg_vec<=9998] - np.median(outerloop_pcg_vec[outerloop_pcg_vec<=9998])))
+        outerloop_stef_std[n] = np.median(np.absolute(outerloop_stef_vec[outerloop_stef_vec<=9998] - np.median(outerloop_stef_vec[outerloop_stef_vec<=9998])))
         print "outerloop_stef_std = ",outerloop_stef_std 
     
 
@@ -468,6 +468,6 @@ def plot_sparsity():
 
 if __name__ == "__main__":
    #plot_kappa_itr(SNR=5)
-   plot_outer_loop(SNR=5)
+   plot_outer_loop(SNR=1000)
    #plot_time()
    #plot_sparsity()
